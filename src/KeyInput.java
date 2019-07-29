ef882a85ec145bbc63c83e2a0aad3a44b7758839
Ahmad Miraz <53442279+Ahmad-Miraz@users.noreply.github.com>
2019-07-29 22:45:48 +0600

    Add files via upload

---
+++ src/KeyInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KeyInput {
+
+	public static void main(String[] args) {
+		String n;
+		Scanner scan = new Scanner(System.in);
+
+		int i = 1;
+		while (!(n = scan.next()).equals("A")) {
+			System.out.println("Input " + i + ": " + n);
+			i++;
+		}
+	}
+}
---
+++ src/Problem2/UapHr.java
@@ -0,0 +1,19 @@
+package Problem2;
+
+import java.util.Scanner;
+
+public class UapHr {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name = scan.next();
+		String id = scan.next();
+		String designation = scan.next();
+		double salary = scan.nextDouble();
+
+		Employee emp = new Employee(name, id, designation, salary);
+		emp.updateSalary(1000);
+		System.out.println(emp.salary);
+		emp.display();
+	}
+}
